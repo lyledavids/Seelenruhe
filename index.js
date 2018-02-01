ea4e5b648882318862a68b1daa3ef1c6d387a239
Lyle Davids <s214311848@nmmu.ac.za>
2018-02-01 03:37:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -31,34 +31,43 @@ const languageStrings = {
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask', 'Welcome to Seelenruhe, your personal assistant for rejuvenating Tai Chi and meditation exercises. Say Introduction if you would like to hear an introduction? or Skip if you dont want to');
+        this.emit(':ask', 'Welcome to Seelenruhe, your personal assistant for rejuvenating Tai Chi and meditation exercises. Say Introduction if you would like to hear an introduction? or say Skip if you dont want to');
     },
     
     'GetIntroduction': function () {
         this.emit('Intro');
     },
     'Intro': function () {
-        this.emit(':ask', 'Tai Chi is is a Chinese martial art practiced for both its defense training and its health benefits. Although originally conceived as a martial art, it is also typically practiced for achieving greater longevity and old age physical health. Some training forms of Tai Chi are especially known for being practiced with slow movements and therefore are great for aging bodies. If you are you ready to start exercising? Say start');
+        this.emit(':ask', 'Tai Chi is a Chinese martial art practiced for both its defense training and its health benefits. Although originally conceived as a martial art, it is also typically practiced for achieving greater longevity and old age physical health. Some training forms of Tai Chi are especially known for being practiced with slow movements and therefore are great for aging bodies. If you are ready to start exercising? Say start');
     },
     
     'NoIntroduction': function () {
         this.emit('NoIntro');
     },
     'NoIntro': function () {
-        this.emit(':ask', 'Okay, no introduction, If you are you ready to start exercising? Say start');
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
+        this.emit(':ask', 'Okay, no introduction, If you are ready to start exercising? Say start');
     },
     
     'ReadyIntent': function () {
         this.emit('YesReady');
     },
     'YesReady': function () {
-        this.emit(':ask', 'Okay, Do you feel full of energy or fatigued today?');
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
+        this.emit(':ask', 'Okay, Are you feeling weak or fatigued today?');
     },
     
     'TheyWeak': function () {
         this.emit('Weakkkk');
     },
     'Weakkkk': function () {
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
         var audioFile = "Let's start our exercise, Keep your stance relaxed and follow the instructors directions. Best of luck and stay healthy <audio src='https://s3-eu-west-1.amazonaws.com/seelenruhestorage/Narration1.mp3' /> Do you like this exercise?";
         this.emit(':ask', audioFile,'try again');
     },
@@ -67,6 +76,9 @@ const handlers = {
         this.emit('NotWeak');
     },
     'NotWeak': function () {
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
         var audioFile = "Let's start our exercise, Keep your stance relaxed and follow the instructors directions. Best of luck and stay healthy <audio src='https://s3-eu-west-1.amazonaws.com/seelenruhestorage/Narration2.mp3' /> Do you like this exercise?";
         this.emit(':ask', audioFile,'try again');
     },
@@ -75,6 +87,10 @@ const handlers = {
         this.emit('UserFeedback');
     },
     'UserFeedback': function () {
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
+        //var audioFile = "<audio src='https://s3-eu-west-1.amazonaws.com/seelenruhestorage/Narration1.mp3' /> Do you like this exercise?";
         this.emit(':tell','Thank you for your feedback. I will remember your preference for the next time');
     },
     
@@ -82,6 +98,9 @@ const handlers = {
         this.emit('NoRea');
     },
     'NoRea': function () {
+        // Create speech output
+        //const speechOutput = this.t('INTRODUCTION_MESSAGE');
+        //this.emit(':tell', speechOutput);
         this.emit(':ask', 'Do you want to go through the essentials again?');
     },
     
@@ -97,7 +116,7 @@ const handlers = {
     },
     
     'ExerciseStart': function () {
-        this.emit(':ask', 'Alright! Before you start your exercise let’s move into the right stance <break time="1s" /> Please stand up straight and follow my instructions <break time="0.3s" /> Lets start <break time="1s" /> Keep your head still and upright and Focus your eyes in the direction of your primary hand. <break time="1s" /> Close your mouth and with your tounge touch the roof of your mouth. <break time="1s" /> Make sure your body is centered and upright. <break time="1s" /> Arch your chest and slightly round your back without hunching it. <break time="1s" /> Loosen the tension around your waist and hips. <break time="1s" /> Relax and lower your shoulders and your elbows. <break time="1s" /> Extend your fingers but keep your wrists relaxed. <break time="0.3s" />Now we can continue to the workout. Say the word ready to start the workout?');
+        this.emit(':ask', 'Alright! Before you start your exercise let’s move into the right stance <break time="1s" /> Please stand up straight and follow my instructions <break time="0.3s" /> Lets start <break time="1s" /> Keep your head still and upright and Focus your eyes in the direction of your primary hand. <break time="1s" /> Close your mouth and with your tounge touch the roof of your mouth. <break time="1s" /> Make sure your body is centered and upright. <break time="1s" /> Arch your chest and slightly round your back without hunching it. <break time="1s" /> Loosen the tension around your waist and hips. <break time="1s" /> Relax and lower your shoulders and your elbows. <break time="1s" /> Extend your fingers but keep your wrists relaxed. <break time="0.3s" />Now we can continue to the workout. Say ready if you are ready to start the workout?');
     },
 
     'AMAZON.HelpIntent': function () {
